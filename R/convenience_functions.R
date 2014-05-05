cada1614922737550d94256c25e81b6be32c698b
Eric C. Anderson <eric.anderson@noaa.gov>
2014-05-05 14:31:56 -0700

    Modify convenience functions to allow for easy stock-sex and stock-age.

--- R/convenience_functions.R
+++ R/convenience_functions.R
@@ -7,20 +7,22 @@
 
 #' Do the steelhead stock analysis in the current working directory
 #'
-#' @param nsim number of simulations
-#' @param B number of bootstrap replicates
-#' @param DoGSI True means do the GSI simulation
+#' Note that you have to be sure that stock_group_start_col is appropriate for the
+#' input file.  The input file can be modified to do stock-sex or stock-age if desired.
+#' @inheritParams run_boot_gsi_analysis
+#' @param STOCK.DATA.XLSX The basename of the file with the data in it for running the simulation
+#' The function assumes that the file is in the "data_files" directory of the package. 
 #' @export
-do_steelhead_stock <- function(nsim, B, DoGSI=FALSE) {
+do_steelhead <- function(STOCK.DATA.XLSX, stock_group_start_col, nsim, B, DO_GSI_ON_PROP) {
   dat.dir = system.file("data_files", package="lowergranite", mustWork=T)
   run_boot_gsi_analysis(
     W = NULL,
-    stock_group_start_col = 14,
+    stock_group_start_col = stock_group_start_col,
     DAT.DIR = system.file("data_files", package="lowergranite", mustWork=T),
     WORK.DIR = getwd(),
-    STOCK.DATA.XLSX = file.path(dat.dir, "SH11SIMPOPstock.xlsx"),
+    STOCK.DATA.XLSX = file.path(dat.dir, STOCK.DATA.XLSX),
     collaps = c(1,1,1,1,1,2,2,2,3,4,5,6,7,8,9,10,10,10,10,11,11,11,11,11,11,11,11),
-    DO_GSI_ON_PROP  = DoGSI,
+    DO_GSI_ON_PROP  = DO_GSI_ON_PROP,
     GSISIM = gsi_simBinaryPath(),
     GSI_SEEDS = c(NA, NA),
     BLFILE  = file.path(dat.dir, "sthd_base_v3_187.txt"),
@@ -37,20 +39,22 @@ do_steelhead_stock <- function(nsim, B, DoGSI=FALSE) {
 
 #' Do the chinook stock analysis in the current working directory
 #'
-#' @param nsim number of simulations
-#' @param B number of bootstrap replicates
-#' @param DoGSI True means do the GSI simulation
+#' Note that you have to be sure that stock_group_start_col is appropriate for the
+#' input file.  The input file can be modified to do stock-sex or stock-age if desired.
+#' @inheritParams run_boot_gsi_analysis
+#' @param STOCK.DATA.XLSX The basename of the file with the data in it for running the simulation
+#' The function assumes that the file is in the "data_files" directory of the package.
 #' @export
-do_chinook_stock <- function(nsim, B, DoGSI=FALSE) {
+do_chinook <- function(STOCK.DATA.XLSX, stock_group_start_col, nsim, B, DO_GSI_ON_PROP) {
   dat.dir = system.file("data_files", package="lowergranite", mustWork=T)
   run_boot_gsi_analysis(
     W = NULL,
-    stock_group_start_col = 14,
+    stock_group_start_col = stock_group_start_col,
     DAT.DIR = system.file("data_files", package="lowergranite", mustWork=T),
     WORK.DIR = getwd(),
-    STOCK.DATA.XLSX = file.path(dat.dir, "CH11SIMPOP.xlsx"),
+    STOCK.DATA.XLSX = file.path(dat.dir, STOCK.DATA.XLSX),
     collaps = c(1,1,2,3,4,5,6,7,8,9,10,11,11,11),
-    DO_GSI_ON_PROP  = DoGSI,
+    DO_GSI_ON_PROP  = DO_GSI_ON_PROP,
     GSISIM = gsi_simBinaryPath(),
     GSI_SEEDS = c(NA, NA),
     BLFILE  = file.path(dat.dir, "chnk_base_v3_180.txt"),
@@ -62,3 +66,6 @@ do_chinook_stock <- function(nsim, B, DoGSI=FALSE) {
     reset_booty_seed = 0
   )
 }
+
+
+
